cbaf951b04a0291770605bfeea55a11acc57c193
unknown <infalsabir@gmail.com>
2024-08-09 17:51:03 +0500

    Added Streamlit integration for Gemini Chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,8 @@
+# using Streamlit
+import streamlit as st
 import google.generativeai as genai
+
+# Configure the global Generative AI API
 API_KEY = "AIzaSyCLFYTD2P4S8KXLU0r7lhCvJ3C0y-reoSI"
 genai.configure(api_key=API_KEY)
 
@@ -10,6 +14,22 @@ def getResponseFromModel(user_input):
     response = model.generate_content(user_input)
     return response.text
 
-user_input = input("Enter Your Prompt = ")
-output = getResponseFromModel(user_input)
-print(output)
+
+# Streamlit UI
+st.title("Generative AI Model")
+
+# Get user input through a text input box
+user_input = st.text_input("Enter your text here")
+
+# craete a button that when clicked, will generate the response
+if st.button("Generate Response"):
+    if user_input:
+        output = getResponseFromModel(user_input)
+        st.write(f"Chatbot Response: {output}")
+    else:
+            st.write("Please enter some text to generate a response")
+
+
+# user_input = input("Enter Your Prompt = ")
+# output = getResponseFromModel(user_input)
+# print(output)
